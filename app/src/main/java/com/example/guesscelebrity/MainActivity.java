5c3ec53ef0cdc7498490dd0a2f471c348f3b0cd0
Amoxe200 <aimanaqari@gmail.com>
2020-11-01 13:19:18 +0100

    This version contains : generating the image generating the names and store them in an ArrayList generating the image Links and pick a random one

--- app/src/main/java/com/example/guesscelebrity/MainActivity.java
+++ app/src/main/java/com/example/guesscelebrity/MainActivity.java
@@ -3,11 +3,13 @@ package com.example.guesscelebrity;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.content.DialogInterface;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.renderscript.ScriptGroup;
+import android.text.LoginFilter;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -27,9 +29,8 @@ import java.util.concurrent.ExecutionException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
     ImageView imageView;
-
 
     public class DownloadWebContent extends AsyncTask<String, Void, String>
     {
@@ -92,7 +93,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void findName(String result)
+    public ArrayList<String> findName(String result)
     {
         ArrayList<String> listName = new ArrayList<String>();
         Pattern pattern = Pattern.compile("alt=\"(.*?)\"");
@@ -102,59 +103,85 @@ public class MainActivity extends AppCompatActivity {
         {
             listName.add(matcher.group(1));
         }
-            Log.i("Names", String.valueOf(listName));
+         return listName;
     }
 
-    public String findLink(String result)
+    public ArrayList<String> findLink(String result)
     {
         ArrayList<String> link = new ArrayList<>();
         Pattern p = Pattern.compile("thumbnail(.*?)\"");
         Matcher matcher = p.matcher(result);
-
         while (matcher.find())
         {
             link.add(matcher.group(1));
         }
             Log.i("Names", String.valueOf(link));
+        return link;
+    }
 
-        // pick a random link from the ArrayList
+    public String returnRandom (ArrayList<String> links)
+    {
+        Random rand = new Random();
+        int randomIndex = rand.nextInt(links.size());
 
-        Random ran = new Random();
-        int randomIndex = ran.nextInt(link.size());
-        String randomElement = link.get(randomIndex);
+        if (randomIndex == 10)
+        {
+           randomIndex =  rand.nextInt(links.size());
+        }
 
+        String randomElement = links.get(randomIndex);
         return randomElement;
     }
 
-
-    static void linkTheNameAndTheLink(ArrayList<String> link1)
+    public String mannageString(String theOne)
     {
+        String name = "";
+        String firstName;
+        String secondName;
+        String thirdName;
+        String returnedOne = "";
 
-    }
-
-    public void clickedOne(View view)
-    {
-        Log.i("Tag", view.getTag().toString());
-        if (view.getTag().toString().equals(view.getTag()))
+        Pattern p = Pattern.compile("/(.*?)-photo");
+        Matcher matcher = p.matcher(theOne);
+        while (matcher.find())
         {
-            Toast.makeText(this, "btn1", Toast.LENGTH_SHORT).show();
+            name = matcher.group(1);
         }
+
+        String[] splitedString = name.split("-");
+
+        if (splitedString.length <= 1)
+            {
+                firstName = splitedString[0];
+                returnedOne = firstName.substring(0,1).toUpperCase() + firstName.substring(1);
+            }
+
+        else if (splitedString.length > 1)
+            {
+                firstName = splitedString[0];
+                secondName = splitedString[1];
+                returnedOne = secondName.substring(0,1).toUpperCase() + secondName.substring(1) + " " + firstName.substring(0, 1).toUpperCase() + firstName.substring(1);
+             }
+
+        Log.i("test3", returnedOne);
+        return returnedOne;
     }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        DownloadWebContent downlaod = new DownloadWebContent();
-        DownloadWebImages downloadWebImages = new DownloadWebImages();
-        Bitmap myImage;
 
+        DownloadWebContent downlaod = new DownloadWebContent();
+        final DownloadWebImages downloadWebImages = new DownloadWebImages();
+        Bitmap myImage;
 
         String result = "";
         String imageLink;
         String firstUrl = "https://famousheights.net/5ft-5in-165-cm";
-        String urlForImage = "https://famousheights.net/thumbnail";
+        final String urlForImage = "https://famousheights.net/thumbnail";
         Button btn1 = findViewById(R.id.btn1);
         Button btn2 = findViewById(R.id.btn2);
         Button btn3 = findViewById(R.id.btn3);
@@ -169,8 +196,17 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         }
 
-        String scondOne = findLink(result);
+        // after the connection of the app with the website i get the lists of the names and store it in an arrayList
+        ArrayList<String> listName = findName(result);
+        Log.i("test1", String.valueOf(listName));
+
+        // here the i call the function that returns the random link and splited string
+        final ArrayList<String> listLink = findLink(result);
+        String scondOne = returnRandom(listLink);
         String thirdOne = urlForImage + scondOne;
+
+        final String theOne = mannageString(scondOne);
+        Log.i("theOne", theOne);
 
         try {
             myImage = downloadWebImages.execute(thirdOne).get();
@@ -179,6 +215,144 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         }
 
-        findName(result);
+        // change the textView of the buttons
+
+        Random random = new Random();
+        int randNumber = random.nextInt(4);
+        Log.i("randomNumber", String.valueOf(randNumber));
+
+        // this is where i change the textView of my buttons
+        if (randNumber == 0)
+        {
+            btn1.setText(theOne);
+
+            btn2.setText(listName.get(random.nextInt(listName.size())));
+                if (btn1.getText().toString().equals(btn2.getText().toString()))
+                {
+                    btn2.setText(listName.get(random.nextInt(listName.size())));
+                }
+
+            btn3.setText(listName.get(random.nextInt(listName.size())));
+            if (btn1.getText().toString().equals(btn3.getText().toString()))
+            {
+                btn3.setText(listName.get(random.nextInt(listName.size())));
+            }
+
+            btn4.setText(listName.get(random.nextInt(listName.size())));
+            if (btn1.getText().toString().equals(btn4.getText().toString()))
+            {
+                btn4.setText(listName.get(random.nextInt(listName.size())));
+            }
+        }
+        else if (randNumber == 1)
+        {
+            btn2.setText(theOne);
+
+            btn1.setText(listName.get(random.nextInt(listName.size())));
+            if (btn2.getText().toString().equals(btn1.getText().toString()))
+            {
+                btn1.setText(listName.get(random.nextInt(listName.size())));
+            }
+
+            btn3.setText(listName.get(random.nextInt(listName.size())));
+            if (btn2.getText().toString().equals(btn3.getText().toString()))
+            {
+                btn3.setText(listName.get(random.nextInt(listName.size())));
+            }
+
+            btn4.setText(listName.get(random.nextInt(listName.size())));
+            if (btn2.getText().toString().equals(btn4.getText().toString()))
+            {
+                btn4.setText(listName.get(random.nextInt(listName.size())));
+            }
+        }
+        else if (randNumber == 2)
+        {
+            btn3.setText(theOne);
+
+            btn1.setText(listName.get(random.nextInt(listName.size())));
+            if (btn3.getText().toString().equals(btn1.getText().toString()))
+            {
+                btn1.setText(listName.get(random.nextInt(listName.size())));
+            }
+
+            btn2.setText(listName.get(random.nextInt(listName.size())));
+            if (btn3.getText().toString().equals(btn2.getText().toString()))
+            {
+                btn2.setText(listName.get(random.nextInt(listName.size())));
+            }
+
+            btn4.setText(listName.get(random.nextInt(listName.size())));
+            if (btn3.getText().toString().equals(btn4.getText().toString()))
+            {
+                btn4.setText(listName.get(random.nextInt(listName.size())));
+            }
+        }
+        else if (randNumber == 3)
+        {
+            btn4.setText(theOne);
+
+            btn1.setText(listName.get(random.nextInt(listName.size())));
+            if (btn4.getText().toString().equals(btn1.getText().toString()))
+            {
+                btn1.setText(listName.get(random.nextInt(listName.size())));
+            }
+
+            btn2.setText(listName.get(random.nextInt(listName.size())));
+            if (btn4.getText().toString().equals(btn2.getText().toString()))
+            {
+                btn2.setText(listName.get(random.nextInt(listName.size())));
+            }
+
+            btn3.setText(listName.get(random.nextInt(listName.size())));
+            if (btn4.getText().toString().equals(btn3.getText().toString()))
+            {
+                btn3.setText(listName.get(random.nextInt(listName.size())));
+            }
+
+        }
+        
+        View.OnClickListener onClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String newOne = returnRandom(listLink);
+                String linkOfImage = urlForImage + newOne;
+                Log.i("newOne", newOne);
+                Log.i("newOne", linkOfImage);
+                String correctAnswer = mannageString(newOne);
+                Log.i("newOne", correctAnswer);
+                Bitmap imageTwo;
+
+                Button button = (Button) v;
+                if (v.getTag().toString().equals(v.getTag()))
+                {
+                        if (theOne == ((Button) v).getText())
+                        {
+                            Toast.makeText(v.getContext(), "Correct Bro congrats", Toast.LENGTH_SHORT).show();
+                                // change the picture
+                            try {
+                                imageTwo = downloadWebImages.execute(linkOfImage).get();
+                                imageView.setImageBitmap(imageTwo);
+
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+
+                        }
+                        else
+                        {
+                            Toast.makeText(MainActivity.this, "Try Again", Toast.LENGTH_SHORT).show();
+                        }
+                }
+            }
+        };
+
+        btn1.setOnClickListener(onClickListener);
+        btn2.setOnClickListener(onClickListener);
+        btn3.setOnClickListener(onClickListener);
+        btn4.setOnClickListener(onClickListener);
+
     }
+
 }
